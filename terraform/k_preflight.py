8046e73c2ddd3739fb17b82e77363e8b46f3a19b
Joe G <je.gg.garcia24@outlook.com>
2026-07-13 17:41:52 -0500

    chore: consolidate release as v1.0.0

--- terraform/k_preflight.py
+++ terraform/k_preflight.py
@@ -160,9 +160,9 @@ def select_inputs(
                 },
                 "events": [
                     {
-                        "name": "Immutable v1.0.4 source",
+                        "name": "Immutable v1.0.0 source",
                         "status": "passed",
-                        "message": "v1.0.4 source context and deployment source passed",
+                        "message": "v1.0.0 source context and deployment source passed",
                     },
                     {
                         "name": "Compartment availability",
